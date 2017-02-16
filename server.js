7ecef93ee0f432baf1595228ee156e2936979921
mizutombo <thomas.shultz52@gmail.com>
2017-02-16 04:48:25 -0800

    commits initial files folders and server.js

---
+++ server.js
@@ -0,0 +1,10 @@
+const app = require('./lib/app');
+require('./lib/connection'); // executes mongoose.connect()
+const http = require('http');
+const port = process.env.PORT || 3000;
+
+const server = http.createServer(app);
+
+server.listen(port, () => {
+	console.log('server running on ', server.address().port);
+});
