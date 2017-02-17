7c980dc4a820853c95bef2bf2a200d905709014d
mizutombo <thomas.shultz52@gmail.com>
2017-02-17 00:35:56 -0800

    user.test.js

---
+++ test/user.test.js
@@ -0,0 +1,31 @@
+const User = require('../lib/models/user');
+const testInvalid = require('./test-invalid')(User);
+const assert = require('chai').assert;
+
+describe('tests user model :', () => {
+
+	it('requires a username', () => {
+		return testInvalid({ password: 'halibut'});
+	});
+
+	it('requires a hash via password', () => {
+		return testInvalid({ username: 'username' });
+	});
+
+	it('is valid with username and password', () => {
+		return new User({ username: 'username', password: 'halibut' }).validate();
+	});
+
+	it('sets has from password and correctly compares', () => {
+		const data = { username: 'username', password: 'halibut' };
+		const user = new User(data);
+
+		assert.isUndefined(user.password);
+		assert.notEqual(user.hash, data.password);
+
+		assert.isTrue(user.comparePassword('halibut'));
+		assert.isFalse(user.comparePassword('not the password'));
+	});
+
+});
+
