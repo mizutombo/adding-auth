ab5532b033132aa056661b00bafc8c95ed7a48a5
mizutombo <thomas.shultz52@gmail.com>
2017-02-17 00:18:01 -0800

    adds test-invalid.js

---
+++ test/test-invalid.js
@@ -0,0 +1,10 @@
+module.exports = function(Model) {
+
+	return (data) => new Model(data)
+		.validate()
+		.then(
+			() => { throw new Error('validation should not have succeeded'), },
+			() => { /* err was expected, nothing to do */ }
+		);
+
+};
